593804df352fe5d9d13336f345d7462f05593ceb
victordelapuente5 <victordelapuente5@gmail.com>
2024-06-13 11:34:50 +0000

    IDX enviroment changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -19,6 +19,10 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "PKief.material-icon-theme"
+      "PKief.material-product-icons"
+      "srcery-colors.srcery-colors"
+      "usernamehw.errorlens"
     ];
 
     # Enable previews
